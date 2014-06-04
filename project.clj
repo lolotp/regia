0f9da9a7445911a2b8f0e9a7192d1b9b494dd223
Phuong <trinhp@garena.com>
2014-06-04 22:42:15 +0800

    initial commit

---
+++ project.clj
@@ -0,0 +1,49 @@
+(defproject
+  regia
+  "0.1.0-SNAPSHOT"
+  :repl-options
+  {:init-ns regia.repl}
+  :dependencies
+  [[ring-server "0.3.1"]
+   [postgresql/postgresql "9.1-901-1.jdbc4"]
+   [ragtime "0.3.6"]
+   [environ "0.5.0"]
+   [com.taoensso/timbre "3.2.1"]
+   [markdown-clj "0.9.44"]
+   [org.clojure/clojure "1.6.0"]
+   [com.taoensso/tower "2.0.2"]
+   [log4j
+    "1.2.17"
+    :exclusions
+    [javax.mail/mail
+     javax.jms/jms
+     com.sun.jdmk/jmxtools
+     com.sun.jmx/jmxri]]
+   [korma "0.3.1"]
+   [selmer "0.6.6"]
+   [lib-noir "0.8.3"]]
+  :ring
+  {:handler regia.handler/app,
+   :init regia.handler/init,
+   :destroy regia.handler/destroy}
+  :ragtime
+  {:migrations ragtime.sql.files/migrations,
+   :database
+   "jdbc:postgresql://localhost/regia?user=db_user_name_here&password=db_user_password_here"}
+  :profiles
+  {:uberjar {:aot :all},
+   :production
+   {:ring
+    {:open-browser? false, :stacktraces? false, :auto-reload? false}},
+   :dev
+   {:dependencies [[ring-mock "0.1.5"] [ring/ring-devel "1.2.2"]],
+    :env {:dev true}}}
+  :url
+  "http://example.com/FIXME"
+  :plugins
+  [[lein-ring "0.8.10"]
+   [lein-environ "0.5.0"]
+   [ragtime/ragtime.lein "0.3.6"]]
+  :description
+  "FIXME: write description"
+  :min-lein-version "2.0.0")
---
+++ src/regia/db/schema.clj
@@ -0,0 +1,9 @@
+(ns regia.db.schema)
+
+(def db-spec
+  {:subprotocol "postgresql"
+   :subname "//localhost/regia"
+   :user "db_user_name_here"
+   :password "db_user_password_here"})
+
+
